de182e21e9fffe003d1a6e302f04b520c9c15139
audrey juarez <audrey1.juarez@gmail.com>
2022-09-26 11:04:47 -0500

    feat: cambios del script

---
+++ script.js
@@ -0,0 +1,8 @@
+
+      console.log("Saoko papi saoko")
+    document.getElementById("generation").style.color = "gray";
+      function colorTitulo(){
+        console.log("Pulso el botón de Generation");
+        document.getElementById("generation").style.color = "red";
+      }
+    
